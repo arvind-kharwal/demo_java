03ddf3807d04f0a2836498d647b4becb84e57d53
Arvind Kharwal <karvind629@gmail.com>
2026-01-30 10:15:47 +0530

    first lab on 30 January 2026

--- Hello.java
+++ Hello.java
@@ -1,6 +1,12 @@
 public class Hello {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        int a = 10;
+        float b = 3.45f;
+        double d = 6.76;
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(d);
+        System.out.println("The numbers are"+a+" "+b+" "+d);
     }
     
 }
---
+++ Second.java
@@ -0,0 +1,16 @@
+class First{
+    public static void main(String[] args) {
+        System.out.println("I am in class First");
+    }
+}
+public class Second{
+    public static void main(String[] args) {
+        System.out.println("I am in class Second");
+    }
+
+}
+class Third{
+    public static void main(String[] args) {
+        System.out.println("I am in class Third");
+    }
+}
