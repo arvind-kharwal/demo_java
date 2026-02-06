1f5566a5fd94b7a4b34a27411a94d186da633d82
Arvind Kharwal <karvind629@gmail.com>
2026-02-06 09:16:26 +0530

    6 February 2026

--- Unit 01/Main1.java
+++ Unit 01/Main1.java
@@ -1,16 +1,3 @@
-class Rectangle{
-    int width, length; // Instance Variables
-    void getdata(int width,int length){
-        this.width = width;
-        this.length = length;
-    }
-    int area(){
-        int a = length*width;
-        return(a);
-    }
-
-}
-
 public class Main1 {
     public static void main(String[] args) {
         Rectangle R = new Rectangle();
---
+++ Unit 01/Rectangle.java
@@ -0,0 +1,12 @@
+class Rectangle{
+    int width, length; // Instance Variables
+    void getdata(int width,int length){
+        this.width = width;
+        this.length = length;
+    }
+    int area(){
+        int a = length*width;
+        return(a);
+    }
+
+}
