4249c03d236c10bb09ee38fb702b44d37e806bf6
Arvind Kharwal <karvind629@gmail.com>
2026-02-10 10:31:09 +0530

    10 February 2026

--- Unit 01/Method Overloading/Main.java
+++ Unit 01/Method Overloading/Main.java
@@ -1,18 +1,19 @@
 class Overload{
-    int add(int a,int b){
-        return(a+b);
-    }
+    // int add(int a,int b){
+    //     return(a+b);
+    // }
     int add(int a,int b,int c){
         return(a+b+c);
     }
-    double add(int a,double b){
-        return(a+b);
+    void add(int a,long b){
+        System.out.println(a);
+        System.out.println(b);
     }
 }
 public class Main {
     public static void main(String[] args) {
         Overload O1 = new Overload();
-        int r= O1.add(10,20);
-        System.out.println(r);
+        O1.add(10,20 );
+        // System.out.println(r);
     }
 }
