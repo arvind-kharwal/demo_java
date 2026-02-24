a19615e0a89615a06939484d99978ac2f2ad5767
Arvind Kharwal <karvind629@gmail.com>
2026-02-24 10:01:14 +0530

    24 February 2026

---
+++ Unit 01/Package/pack1/First.java
@@ -0,0 +1,12 @@
+package pack1;
+class Example{
+    void display(){
+        System.out.println("Display in Example");
+    }
+}
+public class First {
+    public static void main(String[] args) {
+        Example E = new Example();
+        E.display();
+    }
+}
---
+++ Unit 01/Package/pack1/Second.java
@@ -0,0 +1,8 @@
+package pack1;
+
+public class Second {
+    public static void main(String[] args) {
+        Example E2 = new Example();
+        E2.display();
+    }
+}
