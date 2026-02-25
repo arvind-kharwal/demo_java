b4240ab08d0faf67c17d3983bb693e9b22d8ee92
Arvind Kharwal <karvind629@gmail.com>
2026-02-25 12:42:11 +0530

    25 February 2026

---
+++ Unit 02/Exception/Main1.java
@@ -0,0 +1,9 @@
+public class Main1 {
+    static void display(){
+        int r = 6/0;
+        System.out.println(r);
+    }
+    public static void main(String[] args) {
+        display();
+    }
+}
---
+++ Unit 02/Exception/Main2.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+public class Main2 {
+    public static void main(String[] args) {
+        Scanner S = new Scanner(System.in);
+        System.out.println("Enter the numbers:");
+        int a = S.nextInt();
+        int b = S.nextInt();
+        try{
+            int res = a/b;
+            System.out.println("The result is: "+res);
+        }catch(ArithmeticException e){
+            System.out.println("can't divide it by zero");
+        }
+        finally{
+            S.close();
+            System.out.println("End of code");
+        }
+        
+    }
+}
