ab13d20d5759ada4e7481dd772cf1fecbde39ce9
Arvind Kharwal <karvind629@gmail.com>
2026-03-17 10:29:54 +0530

    17 March 2026

---
+++ ST01 Solution/Static/Main.java
@@ -0,0 +1,19 @@
+class Example{
+    static int a = 10;
+    static String name ="Arvind";
+    int b;
+    Example(int b){
+        this.b = b;
+    }
+    static void display(){
+        System.out.println(b);
+    }
+}
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(Example.a);
+        System.out.println(Example.name);
+        Example E1 = new Example(20);
+        E1.display();
+    }
+}
