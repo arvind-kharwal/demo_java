e8499e4be9f28ae46b0d9ad9475a671dab062cc8
Arvind Kharwal <karvind629@gmail.com>
2026-03-24 10:04:21 +0530

    24 March 2026

---
+++ Unit 03/Type Inference/Main.java
@@ -0,0 +1,20 @@
+public class Main {
+    // var x;
+    // var y;
+    // var display(var x,var y)
+    // {
+    //     System.out.println(x,y);
+    // }
+    public static void main(String[] args) {
+        var a = 10;
+        var  b = 3.5;
+        var c = "Hello";
+        var d = true;
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(d);
+
+
+    }
+}
