9fd62e31b72add4ae5959ae09c684c157985a65a
Arvind Kharwal <karvind629@gmail.com>
2026-03-25 11:01:47 +0530

    25 March 2026

---
+++ Unit 03/Functional Interface/Main1.java
@@ -0,0 +1,12 @@
+interface Sample1{
+    void display(String name);
+}
+public class Main1 implements Sample1 {
+    public void display(String name){
+        System.out.println("Thed name is"+name);
+    }
+    public static void main(String[] args) {
+        Main1 M = new Main1();
+        M.display("Java Programming");
+    }
+}
