b26e18ecb3fa8ec10c077980d5ffc9115661d348
Arvind Kharwal <karvind629@gmail.com>
2026-03-25 11:16:53 +0530

    25 March 2026

---
+++ Unit 03/Functional Interface/Main4.java
@@ -0,0 +1,10 @@
+interface Sample4{
+    int sum(int a,int b);
+}
+public class Main4 {
+    public static void main(String[] args) {
+        Sample4 S = (int a,int b)-> a+b;
+        int res = S.sum(10,20);
+        System.out.println(res);
+    }
+}
