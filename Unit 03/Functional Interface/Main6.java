8f98d1faf5b9b3c2db648d9694d99e665db8af88
Arvind Kharwal <karvind629@gmail.com>
2026-03-25 12:42:44 +0530

    25 March 2026

--- Unit 03/Functional Interface/Main6.java
+++ Unit 03/Functional Interface/Main6.java
@@ -1,5 +1,18 @@
-
+import static java.lang.Math.PI;
+import java.util.Scanner;
+interface Example1{
+    double area(int r);
+}
 
 public class Main6 {
-    
+    public static void main(String[] args) {
+        Example1 E1 = (int r)-> PI*r*r;
+        System.out.println("Enter the radius: ");
+        Scanner S= new Scanner(System.in);
+        System.out.println("Enter the radius:");
+        int x = S.nextInt();
+        double res = E1.area(x);
+        System.out.println("The area is: "+res);
+        S.close();
+    }
 }
