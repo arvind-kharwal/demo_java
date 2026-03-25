b267c3e9ba439842df862393ba0da5034021353b
Arvind Kharwal <karvind629@gmail.com>
2026-03-25 10:57:35 +0530

    25 March 2026

--- Unit 03/Functional Interface/Main.java
+++ Unit 03/Functional Interface/Main.java
@@ -1,3 +1,12 @@
-public class Main {
-
+interface Sample{
+    void display();
+}
+public class Main implements Sample {
+    public void display(){
+        System.out.println("I am in display in Class Main");
+    }
+    public static void main(String[] args) {
+        Main M = new Main();
+        M.display();
+    }
 }
