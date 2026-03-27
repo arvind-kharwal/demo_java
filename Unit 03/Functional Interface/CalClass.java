245dcef98f754f0355835ab764966e70b577cb96
Arvind Kharwal <karvind629@gmail.com>
2026-03-27 09:52:06 +0530

    27 March 2026

--- Unit 03/Functional Interface/CalClass.java
+++ Unit 03/Functional Interface/CalClass.java
@@ -17,15 +17,15 @@ public class CalClass {
     public static void main(String[] args) {
         Calc C1 = (double n)->n*n;
         double r1 = C1.square(2.5);
-        System.out.println(r1);
+        System.out.println("The square is: "+r1);
         double r2 = Calc.mul(10,2);
-        System.out.println(r2);
+        System.out.println("The Multiplication: "+r2);
         double r3 = C1.sum(10,20);
-        System.out.println(r3);
+        System.out.println("The sum is: "+r3);
         double r4 = C1.sub(10,20);
-        System.out.println(r4);
+        System.out.println("The subtraction: "+r4);
         double r5 = Calc.div(100,2);
-        System.out.println(r5);
+        System.out.println("The Division: "+r5);
 
     }
 }
