26c9436dc804c31fcdfb821f025b47d3e3e7ea52
Arvind Kharwal <karvind629@gmail.com>
2026-03-27 12:47:39 +0530

    27 March 2026

--- Unit 03/Annotations/Main2.java
+++ Unit 03/Annotations/Main2.java
@@ -1,18 +1,28 @@
+import java.lang.annotation.Repeatable;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 
 @Retention(RetentionPolicy.RUNTIME)
+@Repeatable(Students.class) // class container
 @interface Student1{
     int roll();
     String name();
+}
+
+@Retention(RetentionPolicy.RUNTIME)
+@interface Students{
+    Student1 [] value();
 }
 @Student1(roll=1,name="Arvind")
 @Student1(roll=2,name="Amit")
 
 public class Main2 {
     public static void main(String[] args) {
-        Student S1 = Main2.class.getAnnotation(Student.class);
-        System.out.println(S1.roll());
-        System.out.println(S1.name());
+        Student1 [] S1 = Main2.class.getAnnotationsByType(Student1.class);
+        for(Student1 s:S1)
+        {
+            System.out.println(s.roll());
+            System.out.println(s.name());
+        }
     }
 }
