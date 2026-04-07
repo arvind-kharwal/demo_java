6e9e48c3cfc048a69cb490d2d7e8912fc6ec2677
Arvind Kharwal <karvind629@gmail.com>
2026-04-07 09:55:19 +0530

    7 April 2026

--- Unit 03/Generic Class/Main1.java
+++ Unit 03/Generic Class/Main1.java
@@ -1,8 +1,13 @@
 class Example2<T>{
     void display(T a,T b){
-        
+        System.out.println(a+" "+b);
     }
 }
 public class Main1 {
-    
+    public static void main(String[] args) {
+        Example2<Integer> E1 = new Example2<>();
+        E1.display(10,20);
+        Example2<String> E2 = new Example2<>();
+        E2.display("Java", "CSE18");
+    }
 }
