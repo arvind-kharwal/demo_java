150c78d5645fb8b765d1bfea68f18fc2a369fb9d
Arvind Kharwal <karvind629@gmail.com>
2026-04-08 11:59:30 +0530

    8 April 2026

---
+++ Unit 03/12. Text Block/Main1.java
@@ -0,0 +1,6 @@
+public class Main1 {
+    public static void main(String[] args) {
+        String str = "Hello, I am %s and developed in %d".formatted("Java",1995);
+        System.out.println(str);
+    }
+}
