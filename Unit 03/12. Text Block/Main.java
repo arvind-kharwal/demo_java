ce563c6d16ffcca346b29e25b58577ef5f636f02
Arvind Kharwal <karvind629@gmail.com>
2026-04-08 11:56:05 +0530

    8 April 2026

---
+++ Unit 03/12. Text Block/Main.java
@@ -0,0 +1,33 @@
+public class Main {
+    public static void main(String[] args) {
+        String str1 = """
+                Java Programing
+                """;
+        String str2 = """
+                <!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Page Title</title>
+</head>
+<body>
+    <h1>This is a Heading</h1>
+    <p>This is a paragraph.</p>
+</body>
+</html>
+                """;
+String str3 = """
+        # Simple Python 3 example: Hello World and Variables
+name = "World"
+print(f"Hello, {name}!")
+
+# Basic Loop
+for i in range(3):
+    print(i)
+
+        """;
+System.out.println(str1);
+System.out.println(str2);
+System.out.println(str3);
+    }
+}
