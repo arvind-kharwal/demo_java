20bb74e656519b21322d720072ff012c9f3205cf
Arvind Kharwal <karvind629@gmail.com>
2026-04-10 09:18:24 +0530

    10 April 2026

--- Unit 03/14. Records/Main1.java
+++ Unit 03/14. Records/Main1.java
@@ -1,11 +1,24 @@
-record Employee(int eid, String Name, double sal) {
+import java.util.Scanner;
+
+record Employee(int eid, String name, double sal) {
+    public Employee {
+        // this(0, null, 0);
+        if (eid == 0)
+            throw new IllegalArgumentException();
+    }
 }
 
 public class Main1 {
     public static void main(String[] args) {
-        Employee E1 = new Employee(1, "Arvind", 100.34d);
-        System.out.println(E1);
-        System.out.println(E1.eid());
-        System.out.println(E1.Name());
+        try (Scanner S = new Scanner(System.in)) {
+            System.out.println("Enter the details: ");
+            int eid = S.nextInt();
+            String name = S.nextLine();
+            double sal = S.nextDouble();
+            Employee E1 = new Employee(eid, name, sal);
+            System.out.println(E1);
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+        }
     }
 }
