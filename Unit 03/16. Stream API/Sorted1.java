feabd0cb3135481cde63e9213f3bcb665abfb43f
Arvind Kharwal <karvind629@gmail.com>
2026-04-10 10:26:14 +0530

    10 April 2026

--- Unit 03/16. Stream API/Sorted1.java
+++ Unit 03/16. Stream API/Sorted1.java
@@ -1,3 +1,10 @@
-public class Sorted1 {
+import java.util.*;
 
+public class Sorted1 {
+    public static void main(String[] args) {
+        List<Integer> list = Arrays.asList(12, 2, 30, 4, -5);
+        list.stream()
+                .sorted(Comparator.reverseOrder())
+                .forEach(n -> System.out.print(n + " "));
+    }
 }
