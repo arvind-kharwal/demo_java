451ca37ec1849155056fc51451ca9cb2d3962791
Arvind Kharwal <karvind629@gmail.com>
2026-04-15 11:43:43 +0530

    15 April 2026

---
+++ Unit 04/1. Iterable/Main3.java
@@ -0,0 +1,11 @@
+import java.util.ArrayList;
+import java.util.Collection;
+
+public class Main3 {
+    public static void main(String[] args) {
+        // Collection<Integer> list = Arrays.asList(1, 2, 3, 4);
+        Collection<Integer> list = new ArrayList<>();
+        list.add(10);
+        System.out.println(list);
+    }
+}
