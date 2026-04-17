a297cd176e0266bb4d9a89e236b125275eea68a2
Arvind Kharwal <karvind629@gmail.com>
2026-04-17 10:32:33 +0530

    17 April 2026

--- Unit 04/5. Comparable Interface/Main.java
+++ Unit 04/5. Comparable Interface/Main.java
@@ -1,3 +1,33 @@
-public class Main {
+import java.util.*;
 
+class Student implements Comparable<Student> {
+    int r;
+    String name;
+
+    Student(int r, String name) {
+        this.r = r;
+        this.name = name;
+    }
+
+    public int compareTo(Student S1) {
+        if (r == S1.r)
+            return 0;
+        else if (r > S1.r)
+            return 1;
+        else
+            return -1;
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        List<Student> list = new ArrayList<>();
+        list.add(new Student(3, "Chandan"));
+        list.add(new Student(7, "Dhanush"));
+        list.add(new Student(1, "Arvind"));
+        Collections.sort(list);
+        for (Student i : list) {
+            System.out.println(i.r + " " + i.name);
+        }
+    }
 }
