06a851ff99af155f6ef6493a49bdb28f02f4686d
Arvind Kharwal <karvind629@gmail.com>
2026-04-17 10:00:40 +0530

    17 April 2026

---
+++ Unit 04/3. User Defined List/Main.java
@@ -0,0 +1,26 @@
+import java.util.*;
+
+class Student {
+    int r;
+    String name;
+
+    Student(int r, String name) {
+        this.r = r;
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return r + " " + name;
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        List<Student> list = new ArrayList<>();
+        list.add(new Student(3, "Arvind"));
+        list.add(new Student(1, "Abhi"));
+        list.add(new Student(13, "Bhavya"));
+        System.out.println(list);
+    }
+}
