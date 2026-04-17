5150c9ccf36437a8975d4c075e3f57ff37d9ebcc
Arvind Kharwal <karvind629@gmail.com>
2026-04-17 09:27:34 +0530

    17 April 2026

---
+++ Unit 04/3. List/Main5.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+public class Main5 {
+    public static void main(String[] args) {
+        List<Integer> list = new ArrayList<>();
+        list.add(20);
+        list.add(40);
+        list.add(10);
+        System.out.println("Original list: " + list);
+        Collections.sort(list);
+        System.out.println("Sorted list: " + list);
+        Collections.sort(list, Comparator.reverseOrder());
+        System.out.println("Reverse Sorted list: " + list);
+    }
+}
