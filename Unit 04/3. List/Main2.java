65e97ab1bfc912c961abede39f75b3a226d6a2ec
Arvind Kharwal <karvind629@gmail.com>
2026-04-17 09:16:47 +0530

    17 April 2026

--- Unit 04/3. List/Main2.java
+++ Unit 04/3. List/Main2.java
@@ -1,3 +1,16 @@
-public class Main2 {
+import java.util.*;
 
+public class Main2 {
+    public static void main(String[] args) {
+        List<Integer> list = new ArrayList<>();
+        list.add(10);
+        list.add(20);
+        list.add(40);
+        System.out.println(list);
+        Integer[] arr = list.toArray(new Integer[0]);
+        System.out.println("The array: ");
+        for (Integer i : arr) {
+            System.out.println(i);
+        }
+    }
 }
