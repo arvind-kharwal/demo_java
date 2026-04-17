0a80c00930c7d3d62965b697a721935359013464
Arvind Kharwal <karvind629@gmail.com>
2026-04-17 09:30:27 +0530

    17 April 2026

---
+++ Unit 04/3. List/Main7.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+public class Main7 {
+    public static void main(String[] args) {
+        List<Integer> list = new ArrayList<>();
+        list.add(20);
+        list.add(40);
+        list.add(10);
+        System.out.println("Original list: " + list);
+    }
+}
