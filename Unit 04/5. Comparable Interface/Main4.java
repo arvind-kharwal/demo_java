e2bb440e926169173b371e4b670216c159705408
Arvind Kharwal <karvind629@gmail.com>
2026-04-17 14:37:34 +0530

    17 April 2026

---
+++ Unit 04/5. Comparable Interface/Main4.java
@@ -0,0 +1,17 @@
+class Person {
+    int id;
+    String name;
+    double salary;
+
+    Person(int id, String name, double salary) {
+        this.id = id;
+        this.name = name;
+        this.salary = salary;
+    }
+}
+
+public class Main4 {
+    public static void main(String[] args) {
+
+    }
+}
