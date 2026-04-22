1e05553f99d7ec17d58c2585c256f1195f7e60d0
Arvind Kharwal <karvind629@gmail.com>
2026-04-22 14:06:49 +0530

    22 April 2026

--- Unit 04/6. Comparator/Main1.java
+++ Unit 04/6. Comparator/Main1.java
@@ -10,22 +10,36 @@ class Student1 {
         this.name = name;
         this.marks = marks;
     }
+
+    public int getRoll() {
+        return roll;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getMarks() {
+        return marks;
+    }
+
 }
 
 public class Main1 {
     public static void main(String[] args) {
         List<Student1> list = new ArrayList<>();
         list.add(new Student1(1, "Arvind", 78.56));
-        list.add(new Student1(11, "Chhhaya", 80.56));
+        list.add(new Student1(11, "Ghhhaya", 80.56));
         list.add(new Student1(7, "Bhanu", 91.56));
         // list.sort(Comparator.comparingInt(S -> S.roll));
         // list.sort(Comparator.comparingDouble(S -> S.marks));
         // list.sort(Comparator.comparing(S -> S.name));
-        list.sort(Comparator.comparing()
-                .thenComparing(S -> S.name));
+        list.sort(Comparator.comparing(Student1::getName)
+                .thenComparingInt(Student1::getRoll));
+        // list.sort(Comparator.comparing((S) -> S.name)
+        // .thenComparingInt((S) -> S.age));
         for (Student1 s : list) {
             System.out.println(s.roll + " " + s.name + " " + s.marks);
         }
     }
 }
-
