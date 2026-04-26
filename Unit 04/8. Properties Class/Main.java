4c845679131070a19e9d5ae2e721fba0156649eb
Arvind Kharwal <karvind629@gmail.com>
2026-04-26 13:33:30 +0530

    26 April 2026

--- Unit 04/8. Properties Class/Main.java
+++ Unit 04/8. Properties Class/Main.java
@@ -1,3 +1,14 @@
-public class Main {
+import java.util.*;
+import java.io.*;
 
+public class Main {
+    public static void main(String[] args) throws Exception {
+        FileReader reader = new FileReader("db.properties");
+        Properties p = new Properties();
+        // Add a wrapper around reader object
+        p.load(reader);
+        // access properties data
+        System.out.println(p.getProperty("username"));
+        System.out.println(p.getProperty("password"));
+    }
 }
