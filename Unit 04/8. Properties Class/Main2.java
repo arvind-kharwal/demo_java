0d1b4f32ef2bce7ce01dad2a616d429767d4ffb4
Arvind Kharwal <karvind629@gmail.com>
2026-04-26 13:50:46 +0530

    26 April 2026

--- Unit 04/8. Properties Class/Main2.java
+++ Unit 04/8. Properties Class/Main2.java
@@ -1,3 +1,11 @@
-public class Main2 {
+import java.util.*;
+import java.io.*;
 
+public class Main2 {
+    public static void main(String[] args) throws Exception {
+        Properties p = new Properties();
+        p.setProperty("name", "Arvind");
+        p.setProperty("email", "thekarvind@gmail.com");
+        p.store(new FileWriter("info.properties"), "Properties Example");
+    }
 }
