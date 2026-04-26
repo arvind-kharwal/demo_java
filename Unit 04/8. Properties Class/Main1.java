5f2b1fbdbc79e32acf75902cf0871bd82dcd1ece
Arvind Kharwal <karvind629@gmail.com>
2026-04-26 13:40:16 +0530

    26 April 2026

--- Unit 04/8. Properties Class/Main1.java
+++ Unit 04/8. Properties Class/Main1.java
@@ -1,3 +1,14 @@
-public class Main1 {
+import java.util.*;
+import java.io.*;
 
+public class Main1 {
+    public static void main(String[] args) throws Exception {
+        Properties p = System.getProperties();
+        Set set = p.entrySet();
+        Iterator itr = set.iterator();
+        while (itr.hasNext()) {
+            Map.Entry entry = (Map.Entry) itr.next();
+            System.out.println(entry.getKey() + " = " + entry.getValue());
+        }
+    }
 }
